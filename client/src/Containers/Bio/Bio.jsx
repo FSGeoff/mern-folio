06eda689e96c879d146bbbd7bf7795d14aab8160
Geoff Nix <FullStackGeoff@gmail.com>
2021-02-01 16:29:06 -0500

    added bio page

--- client/src/Containers/Bio/Bio.jsx
+++ client/src/Containers/Bio/Bio.jsx
@@ -5,12 +5,52 @@ const Bio = () => {
 		<div
 			style={{
 				backgroundImage: `url("https://edge.alluremedia.com.au/m/l/2015/06/CodingSnippet.jpg")`,
-				height: "100%",
+				height: "600px",
 				display: "flex",
 				flexDirection: "column",
 			}}
 		>
-			<h1>Bio Page</h1>
+			<div className="spacer" style={{ height: "50px" }}></div>
+			<div
+				className="column is-three-fifths
+is-offset-one-fifth"
+				style={{
+					outline: "solid 1px white",
+					backgroundColor: "black",
+				}}
+			>
+				{" "}
+				<img
+					style={{
+						height: "350px",
+						width: "275px",
+						display: "block",
+						marginLeft: "auto",
+						marginRight: "auto",
+					}}
+					src="https://res.cloudinary.com/fsgeoff/image/upload/v1609190012/C785A719-C5BE-4347-BDD3-1E69FA46DFAD_dbbpb4.jpg"
+					alt="geoff nix"
+				></img>
+				<div
+					className="column is-one-half
+is-offset-one-fourth"
+				>
+					<p
+						style={{
+							color: "white",
+							fontFamily: "sans-serif",
+							fontSize: "22px",
+						}}
+					>
+						Originally, I was a Licensed Master Barber in the State
+						of Georgia. In September of 2017 I was introduced to
+						Salesforce. About 2 years, a software engineering
+						position, and 6 Salesforce Certifications later, I
+						entered Ga Tech Coding Bootcamp. I'm definitely enjoying
+						the journey. So much to learn.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -2,64 +2,62 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div
-			style={{
-				backgroundImage: `url("https://edge.alluremedia.com.au/m/l/2015/06/CodingSnippet.jpg")`,
-				height: "100%",
-				display: "flex",
-				flexDirection: "column",
-			}}
-		>
-            <div
-							className="spacer"
-							style={{ height: "60px" }}
-						></div>
-			<div className="columns" style={{ height: "550px" }}>
-				<div className="column is-6 is-offset-3">
-					{/* <h1
-						style={{
-							fontSize: "50px",
-							fontFamily: "sans-serif",
-							color: "white",
-							textAlign: "center",
-						}}
-					>
-						Hello World
-					</h1> */}
-					<div
-						style={{
-							outline: "solid 1px white",
-							backgroundColor: "black",
-							opacity: "0.75",
-						}}
-					>
-						
-						<p
+		<>
+			<div
+				style={{
+					backgroundImage: `url("https://edge.alluremedia.com.au/m/l/2015/06/CodingSnippet.jpg")`,
+					height: "100%",
+					// display: "flex",
+					// flexDirection: "column",
+				}}
+			>
+				<div className="spacer" style={{ height: "60px" }}></div>
+				<div className="columns" style={{ height: "550px" }}>
+					<div className="column is-6 is-offset-3">
+						<div
 							style={{
-								fontSize: "35px",
-								color: "white",
-								textAlign: "center",
-
-								boxShadow: "inherit",
+								outline: "solid 1px white",
+								backgroundColor: "black",
+								opacity: "0.75",
 							}}
 						>
-							Greetings! My name is Geoff Nix and as you can see, I have a
-							coding background. I am a full stack developer that
-							loves a challenge. This site was built using the
-							full MERN stack. Please explore my site and get to
-							know more about me.
-						</p>
-						<p style={{ textAlign: "center" }}>
-							<a href="/">Resume /</a>
-							<a href="https://www.linkedin.com/in/geoff-nix-070512178/">
-								{" "}
-								LinkedIn
-							</a>
-						</p>
+							<p
+								style={{
+									fontSize: "35px",
+									color: "white",
+									textAlign: "center",
+
+									boxShadow: "inherit",
+								}}
+							>
+								<h1
+									style={{
+										fontSize: "50px",
+										fontFamily: "sans-serif",
+										color: "white",
+										textAlign: "center",
+									}}
+								>
+									Hello World
+								</h1>
+								My name is Geoff Nix and as you can
+								see, I have a coding background. I am a full
+								stack developer that loves a challenge. This
+								site was built using the full MERN stack. Please
+								explore my site and get to know more about me.
+							</p>
+							<p style={{ textAlign: "center" }}>
+								<a href="/">Resume /</a>
+								<a href="https://www.linkedin.com/in/geoff-nix-070512178/">
+									{" "}
+									LinkedIn
+								</a>
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
